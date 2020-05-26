4392419a5c6188747636a5f452ab5cbe24965006
str <manager350@mail.ru>
2020-05-26 19:02:35 +0800

    добавлен вывод по сетке ui, некоторая доработка кода

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,15 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "./apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 
 class App extends React.Component {
-  state = {videos: []};
+  state = {videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit('cats')
+  }
 
 
   onTermSubmit = async term => {
@@ -15,7 +20,15 @@ class App extends React.Component {
       }
     })
 
-    this.setState({videos: response.data.items})
+    this.setState(
+      {videos: response.data.items,
+        selectedVideo: response.data.items[0]
+
+      });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({selectedVideo: video})
   };
 
 
@@ -23,7 +36,19 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit}/>
-        <VideoList videos={this.state.videos}/>
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
